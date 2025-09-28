82fb035cdca847e2499c2f3feabe7cc32e1fd5d5
mia bilal <miabilal035@gmail.com>
2025-09-28 10:28:05 +0300

    encapsulation

--- src/abstraction.kt
+++ src/abstraction.kt
@@ -1,36 +1,48 @@
-//Abstraction in Kotlin
+fun main() {
+    // 1. Instantiate (create an object of) GooglePay
+    var googlePay = GooglePay()
+    println("--- Initiating Google Pay ---")
+    googlePay.PaymentProcess() // Calls the specific GooglePay implementation
+    println("Payment Method: ${googlePay.Payment}\n")
 
 
-fun main(){
+    // 2. Instantiate ApplePay
+    var applePay = ApplePay()
+    println("--- Initiating Apple Pay ---")
+    applePay.PaymentProcess() // Calls the specific ApplePay implementation
+    println("Payment Method: ${applePay.Payment}\n")
 
-    var Pay=GooglePay()
-      Pay.PaymentProcess()
 
+    // 3. Instantiate Card
+    var cardPay = Card()
+    println("--- Initiating Card Payment ---")
+    cardPay.PaymentProcess() // Calls the specific Card implementation
+    println("Payment Method: ${cardPay.Payment}\n")
 }
 
-abstract class Pay{
+// The rest of your classes remain the same
+abstract class Pay {
     abstract var Payment: String
- abstract   fun PaymentProcess()
+    abstract fun PaymentProcess()
 }
 
-class GooglePay:Pay() {
-    override var Payment: String="Google Pay"
-
-
+class GooglePay: Pay() {
+    override var Payment: String = "Google Pay"
     override fun PaymentProcess() {
-        println("Pay with googlePay")
+        println("Paying with Google Pay...")
     }
 }
 
-class ApplePay:Pay(){
-    override var Payment:String= "ApplePay"
+class ApplePay: Pay() {
+    override var Payment: String = "ApplePay"
     override fun PaymentProcess() {
-        println("Pay with ApplePay")
+        println("Paying with ApplePay...")
     }
 }
-class Card:Pay(){
-    override var Payment:String= "Card"
+
+class Card: Pay() {
+    override var Payment: String = "Card"
     override fun PaymentProcess() {
-        println("Pay with Card")
+        println("Paying with Card...")
     }
 }
---
+++ src/encapsulation.kt
@@ -0,0 +1,22 @@
+fun main (){
+    var a : Xyz=Xyz()
+    a.a
+
+
+}
+var Abc= 50;
+private fun tryDo(){
+
+}
+open class Xyz{
+   public var a =10
+   protected fun New(){
+
+    }
+}
+class BBc:Xyz (){
+    var D = a
+    fun abc (){
+        super.New()
+    }
+}
